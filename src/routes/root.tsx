22c3128a3a5a26a970bdc2d54038d1643f87ede7
Aburio <arthur@abur.io>
2023-12-15 23:46:39 +0100

    Trying to fix not found footer and header

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
-import { Footer } from "../components/layout/footer";
-import { Header } from "../components/layout/header";
+import { Footer } from "../components/layout/footer.tsx";
+import { Header } from "../components/layout/header.tsx";
 
 export default function Root() {
   return (
